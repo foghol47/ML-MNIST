844257859429d7f6bfa1f3fc3bc076caa616c806
foghol47 <foghol47@aut.ac.ir>
2022-04-19 09:26:05 +0430

    rename tests folder

--- evaluate.py
+++ evaluate.py
@@ -10,7 +10,7 @@ def main():
     model = keras.models.load_model('models\\model')
     predicts = []
     for i in range(10):
-        image = Image.open('test3\\' + str(i) + '.png').convert('L')
+        image = Image.open('Tests\\test3\\' + str(i) + '.png').convert('L')
         data = 1 - np.asarray(image).reshape(1,28, 28, 1) / 255
         x_pre = model.predict(data)
         x_pre = np.argmax(x_pre, axis=1)
